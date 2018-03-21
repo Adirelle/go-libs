ecce439323ebe0eee26c6925258c59a3897a315e
Adirelle <adirelle+github@gmail.com>
2018-03-21 08:51:01 +0100

    cache: test the Loader option.

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -81,6 +81,43 @@ func TestLoader(t *testing.T) {
 	}
 }
 
+func TestLoaderOption(t *testing.T) {
+
+	ch := make(chan Event, 10)
+	c := NewMemoryStorage(
+		Emitter(ch),
+		Loader(func(k interface{}) (interface{}, error) {
+			t.Logf("Load %v", k)
+			return k.(int) + 10, nil
+		}),
+		Spy(t.Logf),
+	)
+
+	if v, err := c.Get(5); err != nil || v != 15 {
+		t.Error("Get: expected 5, <nil>")
+	}
+
+	if v := c.Len(); v != 1 {
+		t.Error("Len: expected 1")
+	}
+
+	if err := c.Put(5, 6); err != nil {
+		t.Error("Put: expected <nil>")
+	}
+
+	if v, err := c.Get(5); err != nil || v != 6 {
+		t.Error("Get: expected 6, <nil>")
+	}
+
+	if v := c.Remove(5); !v {
+		t.Error("Remove: expected falstruee")
+	}
+
+	if err := c.Flush(); err != nil {
+		t.Error("Flush: expected <nil>")
+	}
+}
+
 func TestEmiter(t *testing.T) {
 
 	ch := make(chan Event, 1)
